fc1033888007936078fefd46e6a09b1514146c95
Mohammed Talha <talhaqqq@gmail.com>
2015-11-14 15:46:23 +0530

    page 134

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -4,15 +4,6 @@ export default Ember.Route.extend({
     return bands.get('content').findBy('slug', params.slug);
   },
 
-  afterModel: function(band){
 
-    var description = band.get('description')
-    if(Ember.isEmpty(description)){
-      this.transitionTo('bands.band.songs');
-    }
-    else {
-      this.transitionTo('bands.band.details');
-    }
-  }
 
 });
